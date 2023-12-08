4dd71aa1adb2a4944b34c5e6e30bd25cfef1d19c
dddboyy <tony4630@naver.com>
2023-12-08 23:13:24 +0900

    Step 2-1
    BG Hue Slider

--- sketch.js
+++ sketch.js
@@ -1,22 +1,30 @@
+// sketch.js
+
 let emitter;
 let repeller;
 let att;
 let isParticleActive = false;
+let hueSlider;
 
 function setup() {
   createCanvas(400, 400);
+
+  // 슬라이더 생성 및 설정
+  hueSlider = createSlider(0, 360, 0, 1);
+  hueSlider.position(10, height + 10);
+
   emitter = new Emitter();
   repeller = new Repeller(width / 2, height / 2);
   att = new Attractor(width / 2, height / 2);
 }
 
 function draw() {
-  background(0);
+  background(hueSlider.value(), 0, 100);
 
   repeller.move(0.5);
 
   if (isParticleActive) {
-    emitter.addParticle(mouseX, mouseY);
+    emitter.addParticle(mouseX, mouseY, hueSlider.value());
   }
 
   emitter.run();
